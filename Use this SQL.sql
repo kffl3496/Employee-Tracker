1c34c9636e2446799d7db8417b91b481f31a04a8
Alex <alexstiles126@gmail.com>
2020-03-23 16:41:19 -0400

    Some final edits on sql file

--- Use this SQL.sql
+++ Use this SQL.sql
@@ -57,41 +57,10 @@ insert into employee (first_name, last_name, role_id, manager_id) values ('Hikar
 -- Run these after you run the top part to rebuild
 
 update employee set manager_id = 1 
-where (id = 3 or id = 5 or id = 9 or id = 10)
+where (id = 3 or id = 5 or id = 9 or id = 10);
 
 update employee set manager_id = 2 
-where (id = 4)
+where (id = 4);
 
 update employee set manager_id = 4
-where (id = 6 or id = 7 or id = 8)
-
-
-
-
--- insert into department (id, name) values (0, 'Level1'); 
--- insert into department (id, name) values (1, 'Level2'); 
--- insert into department (id, name) values (2, 'Level3'); 
-
--- insert into department (id, name) values (3, 'Level4'); 
--- insert into department (id, name) values (4, 'Level5'); 
--- insert into department (id, name) values (5, 'Level6'); 
-
--- insert into role (id, title, salary, department_id) values (0, 'Captain', 100000, 0);
--- insert into role (id, title, salary, department_id) values (1, 'Commander', 90000, 1);
--- insert into role (id, title, salary, department_id) values (2, 'Lt. Commander', 80000, 2);
--- insert into role (id, title, salary, department_id) values (3, 'Lieutenant', 70000, 3);
--- insert into role (id, title, salary, department_id) values (4, 'Ensign', 20000, 4);
-
--- insert into employee (id, first_name, last_name, role_id, manager_id) values (0, 'James', 'Kirk', 0, null);
--- insert into employee (id, first_name, last_name, role_id, manager_id) values (1, 'Jean-Luc', 'Picard', 0, null);
--- insert into employee (id, first_name, last_name, role_id, manager_id) values (2, 'Spock', 'Spock', 1, 0);
--- insert into employee (id, first_name, last_name, role_id, manager_id) values (3, 'William', 'Riker', 1, 1);
--- insert into employee (id, first_name, last_name, role_id, manager_id) values (4, 'Leonard', 'McCoy', 1, 0);
--- insert into employee (id, first_name, last_name, role_id, manager_id) values (5, 'Data', 'Data', 2, 3);
--- insert into employee (id, first_name, last_name, role_id, manager_id) values (6, 'Worf', 'Worf', 3, 3);
--- insert into employee (id, first_name, last_name, role_id, manager_id) values (7, 'Wesley', 'Crusher', 4, 3);
--- insert into employee (id, first_name, last_name, role_id, manager_id) values (8, 'Pavlov', 'Checkov', 4, 0);
--- insert into employee (id, first_name, last_name, role_id, manager_id) values (9, 'Hikaru', 'Sulu', 3, 0);
-
-
-
+where (id = 6 or id = 7 or id = 8);
